88bf914acb85efc57a0b2e2b93c7cb5d150a48e1
jcmdln <jcmdln@gmail.com>
2018-04-25 00:29:34 -0400

    flags.js: Move to strict(er) JSON

---
+++ src/flags.js
@@ -0,0 +1,55 @@
+// flags.js
+
+let Flags = {
+  "help": {
+    "short": "-h",
+    "long":  "--help",
+    "value": false,
+    "about": "Shows this output"
+  },
+  "version": {
+    "short": "-v",
+    "long":  "--version",
+    "value": false,
+    "about": "Show application version"
+  },
+}
+
+let Add = function(Long, Short, Value, About) {
+  Flags[Long] = {
+    "short": '-'+Short,
+    "long":  '--'+Long,
+    "value": Value,
+    "about":  About
+  }
+}
+
+let Parse = function() {
+  let args  = process.argv.slice(2, process.argv.length)
+  let align = 0, pad = ''
+
+  for (f in Flags) {
+    if (f.length > align) { align = f.length }
+  }
+
+  for (f in Flags) {
+    if (f.length < align) {
+      for (i = 0; i < (align - f.length + 4); i++) {
+	pad = pad + ' '
+      }
+    } else {
+      pad = '    '
+    }
+
+    console.log(
+      Flags[f]['short']+',', Flags[f]['long'], pad, Flags[f]['about']
+    )
+
+    pad = ''
+  }
+}
+
+module.exports = {
+  Add:   Add,
+  Parse: Parse
+}
--- src/main.js
+++ src/main.js
@@ -1,72 +1,11 @@
 // main.js
 
 const fs         = require('fs'),
-      wappalyzer = require('wappalyzer')
-
-const usage   = 'Usage: whodis [OPTION] URLs...',
-      flags   = {
-	'help':    [false, '-h', '--help',    'Show this output'],
-	'version': [false, '-v', '--version', 'Show software version'],
-	'file':    [false, '-f', '-file',     'Read list of URLs from a file'],
-	'json':    [false, '-j', '--json',    'Write data to JSON file']
-      }
+      wappalyzer = require('wappalyzer'),
+      flag       = require('./flags.js')
 
 let data    = [],
-    domains = [],
-
-
-///
-/// flags.js
-///
-
-function FlagsHelp() {
-  console.log(usage)
-
-  var align = 0, pad = ''
-  for (f in flags) {
-    if (f.length > align) {
-      align = f.length
-    }
-  }
-
-  for (f in flags) {
-    if (f.length < align) {
-      for (i = 0; i < (align - f.length + 4); i++) { pad = pad + ' ' }
-    } else {
-      pad = '    '
-    }
-    console.log(flags[f][1]+',', f, pad, flags[f][2])
-
-    pad = ''
-  }
-}
-
-function FlagsParse(args) {
-  let al  = args.length,
-      arg = args.slice(2, args.length)
-
-  let fc = flags
-
-  for (i = 0; arg[i]; i++) {
-    console.log(arg)
-    for (f in fc) {
-      console.log(i, f)
-      if (i.indexOf(f[1]) > -1 || i.indexOf(f[2]) > -1) {
-	console.log(i, f)
-      }
-    }
-  }
-
-  console.log(arg)
-  console.log(fc)
-
-  return fc
-}
-
-
-///
-/// Execution
-///
+    domains = []
 
 function Search(url) {
   const w = new wappalyzer(url)
@@ -90,8 +29,13 @@ function Search(url) {
     })
 }
 
+const usage = 'Usage: whodis [OPTION] URLs...'
+
 function main() {
-  FlagsParse(process.argv)
+  var json
+  flag.Add("json", "j", false, "Save data to JSON file")
+
+  flag.Parse()
 }
 
 main()
