6de5b21cc9c8262325b4a62246b5384624ef31fd
jcmdln <jcmdln@gmail.com>
2018-05-02 14:36:17 -0400

    main.js: Cleanup pass

--- src/main.js
+++ src/main.js
@@ -5,35 +5,37 @@ const fs         = require('fs'),
       flag       = require('./flags.js')
 
 let Search = function(url) {
+  // No undefined anything!
+  if (url === '' || typeof url === typeof undefined) {
+    console.log("nope")
+    process.exit(1)
+  }
+
   // If no protocol is provided, assume https
   if (url.indexOf('http') === -1) {
     url = 'https://' + url
   }
 
+  console.log("whodis: looking for software used by '"+url+"'...")
+
   // Set Wappalyzer options
   let options = {
-    debug: false,
-    delay: 50,
-    maxDepth: 3,
-    maxUrls: 10,
-    maxWait: 5000,
-    recursive: true,
-    userAgent: 'whodis',
+    debug:       false,
+    delay:       50,
+    maxDepth:    3,
+    maxUrls:     10,
+    maxWait:     5000,
+    recursive:   true,
+    userAgent:   'whodis',
+    htmlMaxCols: 2000,
+    htmlMaxRows: 2000,
   };
 
   // Instantiate a new Wappalyzer instance
-  let w = new wappalyzer(url, options)
-
-  //
+  const w = new wappalyzer(url, options)
   w.analyze()
     .then(json => {
-      if (json.length === 0) {
-	console.log("whodis: error: no JSON data received")
-	console.log("whodis: exiting...")
-	return
-      } else {
-	process.stdout.write(JSON.stringify(json, null, 2) + '\n')
-      }
+      console.log(JSON.stringify(json, null, 2))
     })
     .catch(error => {
       console.error(error)
@@ -48,10 +50,12 @@ function main() {
   let json = flag.Add("json", "j", "wew.lad.json", "Save data to JSON file")
   let args = flag.Parse()
 
-  // Confirm some arguments were returned
-  if (args.length > 0 ){
-    // Store all retrieved JSON in this array
-    let data = []
+  if (args.length > 0) {
+    if (json['value'] != null) {
+      // Store all retrieved JSON in this array
+      let data = []
+      console.log(cmd['name'], json['value'])
+    }
 
     // Loop over all domains within args
     for (i = 0; i <= args.length; i++) {
@@ -59,5 +63,4 @@ function main() {
     }
   }
 
-  console.log(json['value'])
 }; main()
