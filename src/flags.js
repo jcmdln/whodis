7a7709fd37b990ef0294d41100b812fce25feaf9
jcmdln <jcmdln@gmail.com>
2018-05-02 10:34:31 -0400

    flags.js: Improve help alignment; Add comments

--- src/flags.js
+++ src/flags.js
@@ -1,13 +1,11 @@
 // flags.js
 
-let pkginfo  = require('pkginfo')(module, 'name', 'version', 'description'),
-    pkgname    = module.exports['name'],
-    pkgversion = module.exports['version'],
-    pkgabout   = module.exports['description']
+let pkginfo  = require('pkginfo')(module, 'name', 'version', 'description')
+    pkgversion = module.exports['version']
 
 
-//
-//
+// Define our JSON object which will hold metadata about our commands
+// and their flags. 'help' and 'version' are provided by default
 
 let Cmd = {
   "flags": {
@@ -27,14 +25,13 @@ let Cmd = {
 }
 
 
-//
-//
+// Mechanism for defining commands
 
-let Meta = function(Usage) {
-  Cmd["name"]    = pkgname
+let Meta = function(Name, About, Usage) {
+  Cmd["name"]    = Name
   Cmd["version"] = pkgversion
   Cmd["usage"]   = Usage
-  Cmd["about"]   = pkgabout
+  Cmd["about"]   = About
   return Cmd
 }
 
@@ -49,50 +46,72 @@ let Add = function(Long, Short, Value, About) {
     "value": Value,
     "about": About,
   }
-
   return Cmd['flags'][Long]
 }
 
 
-//
-//
+// This function parses the length of all flags to provide
+// width-respecting help output.
 
 let Help = function() {
+  // Flip 'help's boolean, though this isn't really important as when
+  // this is triggered it will exit.
   Cmd['flags']['help']['value'] = true
 
+  // Print the command name, usage, and about text.
   console.log(
-    'Usage:', pkgname, Cmd['usage']+'\n'
-      + pkgabout+'\n'
+    'Usage:', Cmd['name'], Cmd['usage']+'\n'
+      + Cmd['about']+'\n'
   )
 
-  let align = 0, pad = ''
+  // Store our initial values for alignment and padding.
+  let align = [0, 0], pad = ['', '']
 
+  // Update align to store the length of the longest short/long flags
   for (f in Cmd['flags']) {
-    if (f.length > align) {
-      align = f.length }
+    if (Cmd['flags'][f]['short'].length > align[0]) {
+      align[0] = Cmd['flags'][f]['short'].length
+    }
+    if (Cmd['flags'][f]['long'].length > align[1]) {
+      align[1] = Cmd['flags'][f]['long'].length
+    }
   }
 
+  //
   for (f in Cmd['flags']) {
-    if (f.length < align) {
-      for (i = 0; i < (align - f.length + 4); i++) {
-	pad = pad + ' '
+    let flag_short = Cmd['flags'][f]['short'],
+	flag_long  = Cmd['flags'][f]['long']
+
+    if (flag_short.length < align[0]) {
+      for (i = 0; i < (align[0] - flag_short.length + 1); i++) {
+	pad[0] = pad[0] + ' '
       }
     } else {
-      pad = '    '
+      pad[0] = ' '
+    }
+
+    if (flag_long.length < align[1]) {
+      for (i = 0; i < (align[1] - flag_long.length + 4); i++) {
+	pad[1] = pad[1] + ' '
+      }
+    } else {
+      pad[1] = '    '
     }
 
     console.log(
-      '    '+Cmd['flags'][f]['short']+',', Cmd['flags'][f]['long'], pad,
+      '    ' + flag_short + ',' + pad[0] + flag_long + pad[1],
       Cmd['flags'][f]['about']
     )
 
-    pad = '';
+    // Reset pad
+    pad = ['', ''];
   }
 }
 
 
-//
-//
+// This is our argument parser which reads all arguments from the index
+// of 2 and later, then updates the values and returns non-flag arguments
+// for handling in personal logic.
 
 let Parse = function() {
   let args = process.argv.slice(2, process.argv.length)
@@ -109,7 +128,7 @@ let Parse = function() {
   // Check for '-v|--version', print version and exit.
   if (args.indexOf(Cmd['flags']['version']['short']) > -1  ||
       args.indexOf(Cmd['flags']['version']['long']) > -1) {
-    console.log(pkgname, '- v'+pkgversion)
+    console.log(Cmd['name'], '- v'+pkgversion)
     process.exit()
   }
 
@@ -155,10 +174,12 @@ let Parse = function() {
   return parg
 }
 
-// Here we will expose certain items for user consumption.
+
+// Expose only the needed functions in order to create commands and
+// flags.
+
 module.exports = {
   Add:   Add,
-  Cmd:   Cmd,
   Meta:  Meta,
   Parse: Parse,
 }
--- src/main.js
+++ src/main.js
@@ -4,38 +4,35 @@ const fs         = require('fs'),
       wappalyzer = require('wappalyzer'),
       flag       = require('./flags.js')
 
-let data    = [],
-    domains = []
-
-function Search(url) {
+let Search = function(url) {
   // If no protocol is provided, assume https
   if (url.indexOf('http') === -1) {
     url = 'https://' + url
   }
 
-  const options = {
+  // Set Wappalyzer options
+  let options = {
     debug: false,
-    delay: 100,
+    delay: 50,
     maxDepth: 3,
     maxUrls: 10,
     maxWait: 5000,
     recursive: true,
-    userAgent: 'Wappalyzer',
-    htmlMaxCols: 2000,
-    htmlMaxRows: 2000,
+    userAgent: 'whodis',
   };
 
-  const w = new wappalyzer(url, options)
+  // Instantiate a new Wappalyzer instance
+  let w = new wappalyzer(url, options)
 
+  //
   w.analyze()
     .then(json => {
       if (json.length === 0) {
 	console.log("whodis: error: no JSON data received")
 	console.log("whodis: exiting...")
-	process.exit()
+	return
       } else {
 	process.stdout.write(JSON.stringify(json, null, 2) + '\n')
-	process.exit(0)
       }
     })
     .catch(error => {
@@ -45,11 +42,21 @@ function Search(url) {
 }
 
 function main() {
-  let cmd  = flag.Meta("[OPTION] URLs...")
-  let json = flag.Add("json", "j", "export.json", "Save data to JSON file")
+  // Define our command and flags
+  let cmd  = flag.Meta("whodis", "Use Wappalyzer from the command line",
+		       "[OPTION] URLs...")
+  let json = flag.Add("json", "j", "wew.lad.json", "Save data to JSON file")
   let args = flag.Parse()
 
-  for (i = 0; i <= args.length; i++) {
-    Search(args[i])
+  if (args.length > 0 ){
+    // Store all retrieved JSON in this array
+    let data = []
+
+    // Loop over all domains within args
+    for (i = 0; i <= args.length; i++) {
+      Search(args[i])
+    }
   }
+
+  console.log(json['value'])
 }; main()
