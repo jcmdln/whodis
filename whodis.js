cdec20a4913773364262388e6b061d0ccb6702d9
jcmdln <jcmdln@gmail.com>
2018-06-12 14:18:52 -0400

    Cleanup logic; prepare for adding threads

--- whodis.js
+++ whodis.js
@@ -3,35 +3,36 @@
 const fs         = require('fs'),
       flag       = require('flags'),
       json2csv   = require('json2csv').parse,
+      os         = require("os"),
+      threads    = require('threads'),
       wappalyzer = require('wappalyzer')
 
-let cmd  = flag.Cmd("whodis", "Discover software used by websites", "[OPTION] URLs...")
-let f    = flag.Add("file", "f", "domains.txt", "Read domains from txt file")
-let c    = flag.Add("csv",  "c", "", "Save data to CSV file")
-let j    = flag.Add("json", "j", "", "Save data to JSON file")
-let args = flag.Parse()
-
-let data   = [],
+let cpus   = os.cpus().length,
+    data   = [],
     fields = ['url'],
-    cols   = {fields},
     queue  = []
+
+let cmd  = flag.Cmd("whodis", "Discover software used by websites", "[OPTION] URLs..."),
+    d    = flag.Add("--debug", "-d", false, "Enable debugging"),
+    f    = flag.Add("--file",  "-f", "",    "Read domains from txt file"),
+    c    = flag.Add("--csv",   "-c", "",    "Save data to CSV file"),
+    j    = flag.Add("--json",  "-j", "",    "Save data to JSON file"),
+    args = flag.Parse()
 
 function Search(url) {
   if (url.indexOf('http') === -1) {
     url = 'https://' + url
   }
 
-  const options = {
-    debug:       false,
-    delay:       1000,
-    maxDepth:    3,
-    maxUrls:     9,
-    maxWait:     5000,
-    recursive:   true,
-    userAgent:   'WhoDis'
-  }
-
-  queue.push(new wappalyzer(url, options).analyze())
+  queue.push(new wappalyzer(url, {
+    debug:     d.value,
+    delay:     100,
+    maxDepth:  3,
+    maxUrls:   12,
+    maxWait:   5000,
+    recursive: true,
+    userAgent: 'WhoDis'
+  }).analyze())
 }
 
 function Work() {
@@ -45,9 +46,9 @@ function Work() {
 	  let value = ""
 	  if (results[i]["applications"][a]["version"] != "") {
 	    value = results[i]["applications"][a]["version"]
-	  } // else {
-	  //   value = "yes"
-	  // }
+	  } else {
+	    value = "yes"
+	  }
 
 	  tgt[results[i]["applications"][a]["name"]] = value
 	  fields.push(results[i]["applications"][a]["name"])
@@ -61,7 +62,7 @@ function Work() {
       } else {
 	if (c.value != "") {
 	  console.log("whodis: writing to", c.value)
-	  fs.writeFileSync(c.value, json2csv(data, cols) + '\n', 'utf8')
+	  fs.writeFileSync(c.value, json2csv(data, {fields}) + '\n', 'utf8')
 	}
 
 	if (j.value != "") {
@@ -78,33 +79,29 @@ function Work() {
     })
 }
 
-if (f.value != "") {
-  console.log("whodis: reading domains from", f.value + "...")
-  let File = fs.readFileSync(f.value).toString().split("\n")
+function main() {
+  let Targets = [],
+      Threads = []
 
-  for (i in File) {
-    if (File[i].indexOf(".") > -1) {
-      Search(File[i])
-    } else {
-      console.log("whodis:", File[i], "is invalid")
-      process.exit(1)
-    }
+  if (f.value != "") {
+    console.log("whodis: reading domains from", f.value + "...")
+    Targets = fs.readFileSync(f.value).toString().split("\n")
+  } else if (args.length > 0) {
+    console.log("whodis: reading domains from arguments...")
+    Targets = args;
+  } else {
+    console.log("whodis: No URLs passed! Exiting...")
+    process.exit(0)
+  }
+
+  for (i in Targets) {
+    if (Targets[i].indexOf(".") > -1) {
+      Search(Targets[i])
+    } // else {
+    //   console.log()
+    //   process.exit(1)
+    // }
   }
 
   Work()
-} else if (args.length > 0) {
-  console.log("whodis: queuing domains...")
-  for (i in args.length) {
-    if (args[i].indexOf(".") > -1) {
-      Search(args[i])
-    } else {
-      console.log("whodis:", args[i], "is invalid")
-      process.exit(1)
-    }
-  }
-
-  Work()
-} else {
-  console.log("whodis: No URLs passed! Exiting...")
-  process.exit(0)
-}
+}; main()
