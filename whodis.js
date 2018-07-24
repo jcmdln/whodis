5c65b6b1d1ad518eca10e438b4e9c5d5a5cdc9b0
jcmdln <jcmdln@gmail.com>
2018-07-24 14:40:13 -0400

    Various logic improvements

--- whodis.js
+++ whodis.js
@@ -4,7 +4,6 @@ const fs         = require('fs'),
       flag       = require('flags'),
       json2csv   = require('json2csv').parse,
       os         = require("os"),
-      spawn      = require('threads').spawn,
       wappalyzer = require('wappalyzer')
 
 let cmd  = flag.Cmd("whodis", "Discover software used by websites", "[OPTION] URLs..."),
@@ -14,27 +13,33 @@ let cmd  = flag.Cmd("whodis", "Discover software used by websites", "[OPTION] UR
     j    = flag.Add("--json",  "-j", "",    "Save data to JSON file"),
     args = flag.Parse()
 
-let data   = [],
-    fields = [],
-    urls   = [],
+let urls   = [],
     queue  = []
 
 let options = {
-  debug:     d.value,
-  delay:     500,
-  maxDepth:  3,
-  maxUrls:   10,
-  maxWait:   5000,
-  recursive: true,
-  userAgent: 'WhoDis '+cmd['version']
+  debug:       d.value,
+  delay:       500,
+  // htmlMaxCols: 2000,
+  // htmlMaxRows: 2000,
+  maxDepth:    3,
+  maxUrls:     10,
+  maxWait:     10000,
+  recursive:   true,
+  userAgent:   'WhoDis '+cmd['version']
 }
 
-function Get(queue) {
+function Get() {
+  let data   = [],
+      fields = []
+
+  console.log("whodis: Checking domain(s) for known software...")
+
   Promise.all(queue)
     .then(results => {
       for (i = 0; i < results.length; i++) {
-	let r = {}
-	r["url"]  = results[i]["urls"][0]
+	let r = {
+	  "url": results[i]["urls"][0]
+	}
 
 	for (a = 0; a < results[i]["applications"].length; a++) {
 	  let value = ""
@@ -53,80 +58,72 @@ function Get(queue) {
 
 	data.push(r)
       }
+    })
 
-      if ((j.value === "") && (c.value === "")) {
-	process.stdout.write(JSON.stringify(data, null, 2) + '\n')
+    .finally(function() {
+      if (j.value === "" && c.value === "") {
+        process.stdout.write(JSON.stringify(data, null, 2) + '\n')
       } else {
-	if (c.value != "") {
-	  fields.sort()
-	  fields.unshift('url')
+        if (c.value != "") {
+          fields.sort()
+          fields.unshift('url')
 
-	  console.log("whodis: writing to", c.value)
-	  fs.writeFileSync(c.value, json2csv(data, {fields}) + '\n', 'utf8')
-	}
+          console.log("whodis: writing to", c.value)
+          fs.writeFileSync(c.value, json2csv(data, {fields}) + '\n', 'utf8')
+        }
 
-	if (j.value != "") {
-	  console.log("whodis: writing to", j.value)
-	  fs.writeFileSync(j.value, JSON.stringify(data, null, 2) + '\n', 'utf8')
-	}
+        if (j.value != "") {
+          console.log("whodis: writing to", j.value)
+          fs.writeFileSync(j.value, JSON.stringify(data, null, 2) + '\n', 'utf8')
+        }
       }
 
       process.exit(0)
     })
+
     .catch(error => {
       process.stderr.write('whodis:', error + '\n')
       process.exit(1)
     })
 }
 
-if (d.value === true) {
-  process.stdout.write(JSON.stringify(cmd, null, 2) + '\n')
-}
-
-if (f.value != "") {
-  console.log("whodis: main: Reading domains from", f.value + "...")
-  urls = fs.readFileSync(f.value).toString().split("\n")
-} else {
-  if (args.length > 0) {
-    console.log("whodis: main: Reading domains from arguments...")
-    urls = args
-  } else {
-    console.log("whodis: main: No URLs passed! Exiting...")
-    process.exit(1)
+function main() {
+  if (d.value === true) {
+    process.stdout.write(JSON.stringify(cmd, null, 2) + '\n')
   }
-}
 
-if (urls.length <= 0) {
-  console.log("whodis: main: No URLs passed! Exiting...")
-  process.exit(1)
-}
-
-if (urls.length > 1000) {
-  for (let u = 0; u < 1000; u++) {
-    let url = urls[u]
-
-    if (url.includes(".") === true) {
-      if (url.includes('http') === false) {
-	url = 'https://' + url
-      }
-
-      queue.push(new wappalyzer(url, options).analyze())
+  if (f.value.length > 0) {
+    console.log("whodis: Reading domain(s) from", f.value + "...")
+    urls = fs.readFileSync(f.value).toString().split("\n")
+  } else {
+    if (args.length > 0) {
+      console.log("whodis: Reading domain(s) from arguments...")
+      urls = args
+    } else {
+      console.log("whodis: No URLs passed! Exiting...")
+      process.exit(1)
     }
   }
 
-  Get(queue)
-} else {
+  if (urls.length < 1) {
+    console.log("whodis: No URLs passed! Exiting...")
+    process.exit(1)
+  } else {
+    console.log("whodis: Added", urls.length, "domain(s)")
+    process.setMaxListeners(urls.length)
+  }
+
   for (u in urls) {
     let url = urls[u]
 
     if (url.includes(".") === true) {
       if (url.includes('http') === false) {
-	url = 'https://' + url
+        url = 'https://' + url
       }
 
       queue.push(new wappalyzer(url, options).analyze())
     }
   }
 
-  Get(queue)
-}
+  Get()
+}; main()
