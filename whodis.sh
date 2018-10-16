d1348fb167b173de35ca30e4575bcea28bab9846
jcmdln <jcmdln@gmail.com>
2018-10-16 11:58:32 -0400

    State the error

--- whodis.sh
+++ whodis.sh
@@ -4,11 +4,11 @@ SOURCE="$1"
 DEST="$2"
 
 if [ -z "$SOURCE" ] || [ -z "$DEST" ]; then
+    echo "whodis.sh: error: too few arguments"
     echo "Usage: whodis.sh SOURCE DEST"
     exit
 fi
 
 for i in $(cat "$SOURCE"); do
-    node whodis.js "$i" \
-        -j "$DEST"
+    node whodis.js "$i" -j "$DEST"
 done
