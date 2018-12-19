b7524aa703cede3ba56a788437e812281b449a78
jcmdln <jcmdln@gmail.com>
2018-12-19 18:03:19 -0500

    Cleanup pass

--- README.md
+++ README.md
@@ -1,92 +1,50 @@
 # whodis
 WhoDis uses the existing Wappalyzer nodejs package to detect site
-software from the command line. It is written as cleanly as possible
-in a single file that relies on few imports.
-
-I wrote [flags.js](https://github.com/jcmdln/flags.js) in tandem with
-WhoDis because I wanted unix-style flags parsing in NodeJS, though it
-should be suitable for any NodeJS project.
-
-
-## Why?
-Unlike using the browser extension or stacks-cli, WhoDis allows
-reviewing the software of many sites simultaneously and storing the
-results as a csv or json file. I would like to reach a point where Whodis
-is able to generate reports against millions or tens of millions of
-domains at a time.
+software from the command line.
 
 
 ## Installation
-There are multiple installation methods to choose from:
-
-### From Source
-This is the preferred method since WhoDis is still being heavily
-modified. I personally wouldn't install this as a global program
-quite yet, though.
-
-	$ git clone https://github.com/jcmdln/whodis
-	$ cd whodis
-	$ yarn install
-
-### From a package manager
-I prefer `yarn` but I'll show two examples:
-
-#### Using `npm`
 
 	$ npm install -g git+https://github.com/jcmdln/whodis
 
-#### Using `yarn`
-
-	$ yarn global add git+https://github.com/jcmdln/whodis
-
 
 ## Usage
-A big issue with `whodis` is that due to my haphazard "understanding" of
-NodeJS promises memory is not always relinquished. Due to this, there
-are two strategies for running `whodis` explained below.
 
-### Single domains or small batches
-If running against a single domain or small batches of domains, using
-`whodis` directly is not an issue. For fewer than 50 domains, this is
-the preferred method:
+	$ node whodis.js -h
+	Usage: whodis [OPTION] URLs...
+	Discover software used by websites
 
-	$ node whodis.js techcrunch.com
-	whodis: Reading domain(s) from arguments...
-	whodis: scanning 'techcrunch.com'...
-	whodis: checking for known software...
-	{
-		"url": "http://techcrunch.com/",
-		"Lodash": "4.17.10",
-		"New Relic": "yes",
-		"Nginx": "yes",
-		"Twitter Emoji (Twemoji)": "yes",
-		"Typekit": "1.19.2",
-		"WordPress": " 4.9.8",
-		"PHP": "yes",
-		"MySQL": "yes",
-		"YouTube": "yes"
-	}
+      -h, --help        Show this output
+      -v, --version     Show command version
+      -V, --verbose     Enable verbose output
+      -q, --quiet       Hide all output
+      -d, --debug       Enable Wappalyzer's debug output
+      -f, --file        Read domains from txt file
+      -j, --json        Save data to JSON file
+
+### Single domains (or small batches)
+
+    $ node whodis.js techcrunch.com
+    whodis: reading domain(s) from arguments...
+    whodis: crawling 'https://techcrunch.com'...
+    {
+      "url": "https://techcrunch.com/",
+      "New Relic": "yes",
+      "Nginx": "yes",
+      "WordPress": "5.0.1",
+      "PHP": "yes",
+      "MySQL": "yes",
+      "Lodash": "4.17.11",
+      "Parse.ly": "yes",
+      "Prebid": "yes",
+      "Twitter Emoji (Twemoji)": "yes",
+      "Typekit": "1.19.2",
+      "jQuery": "1.12.4",
+      "jQuery Migrate": "1.4.1",
+      "MediaElement.js": "4.2.6"
+    }
 
 ### Bulk domains
-The shell script takes two arguments: the list of domains to read from,
-and the file to save results to. This could be easily re-created in an
-alternate language such as Python, though peek at the file and re-create
-it if needed. You may use it as follows:
 
 	$ sh whodis.sh list-of-domains.txt whodis-results.csv
 	...
-
-#### Why is this needed at all?
-If you are attempting to collect info on 50 or more domains (ie
-thousands of domains) there is `whodis.sh` which is a shell script to
-act as a frontend. All this does is iterate over each domain with a
-fresh process so we can be sure that memory is being freed. To clarify
-further, this is not my intention. I would very much prefer that running
-`whodis` directly allowed checking against millions of domains, though at
-this time I believe that would require breaking apart the Wappalyzer code
-which I don't believe would be useful.
-
-
-## Contributing
-Assistance, comments, suggestions, and other contributions are welcome!
-If I'm missing a feature, let me know please!
--- whodis.js
+++ whodis.js
@@ -6,6 +6,8 @@ const fs         = require('fs')
 const flag       = require('flags')
 const json2csv   = require('json2csv').parse
 const wappalyzer = require('wappalyzer')
+const browser    = require('./node_modules/wappalyzer/browsers/zombie.js')
+
 
 let cmd     = flag.Cmd("whodis", "Discover software used by websites", "[OPTION] URLs...")
 let Verbose = flag.Add("--verbose", "-V", false, "Enable verbose output")
@@ -15,32 +17,31 @@ let File    = flag.Add("--file",    "-f", "",    "Read domains from txt file")
 let Json    = flag.Add("--json",    "-j", "",    "Save data to JSON file")
 let Args    = flag.Parse()
 
+
 function log(msg) {
     if (!Quiet.value) {
         console.log("whodis:", msg)
     }
-
-    return
 }
+
 
 function verbose(msg) {
     if (Verbose.value && !Quiet.value) {
         console.log("whodis:", msg)
     }
-
-    return
 }
+
 
 function parse(data) {
     verbose("entered 'parse()'")
 
     let parsed = { "url": Object.keys(data["urls"])[0] }
 
-    for (let a = 0; a < data["applications"].length; a++) {
+    for (let a in data["applications"]) {
         let app = data["applications"][a]
         let val = ""
 
-        if (app["version"] != null) {
+        if (app["version"] != null && app["version"] != "") {
             val = app["version"]
         } else {
             val = "yes"
@@ -49,47 +50,46 @@ function parse(data) {
         parsed[app["name"]] = val
     }
 
-    return parsed
+    return JSON.stringify(parsed, null, 2)
 }
+
 
 function save(data) {
     verbose("entered 'save()'")
 
     if (Json.value === "") {
-	console.log(JSON.stringify(data, null, 2))
+        console.log(data)
     } else {
-	if (Json.value != "") {
-	    verbose("checking for existing '"+ Json.value +"'...")
+        if (Json.value != "") {
+            verbose("checking for existing '"+ Json.value +"'...")
 
-	    if (fs.existsSync(Json.value)) {
-		verbose("'"+ Json.value +"' exists. Appending to file...")
+            if (fs.existsSync(Json.value)) {
+                verbose("'"+ Json.value +"' exists. Appending to file...")
 
-		let file = JSON.parse(fs.readFileSync(Json.value))
-		file.push(data)
-		fs.writeFileSync(Json.value, JSON.stringify(file, null, 2) + '\n', 'utf8')
-	    } else {
-		verbose("'"+ Json.value +"' doesn't exist. Creating...")
-		fs.writeFileSync(Json.value, JSON.stringify([data], null, 2) + '\n', 'utf8')
-	    }
-	}
+                let file = JSON.parse(fs.readFileSync(Json.value))
+                file.push(data)
+                fs.writeFileSync(Json.value, data + '\n', 'utf8')
+            } else {
+                verbose("'"+ Json.value +"' doesn't exist. Creating...")
+                fs.writeFileSync(Json.value, data + '\n', 'utf8')
+            }
+        }
     }
-
-    return
 }
+
 
 async function main() {
     let urls = []
     let opts = {
-	chunkSize:   1,
-	debug:       Debug.value,
-	delay:       0,
-	htmlMaxCols: 2000,
-	htmlMaxRows: 2000,
-	maxDepth:    1,
-	maxUrls:     1,
-	maxWait:     5000,
-	recursive:   false,
-	userAgent:   'WhoDis'
+        debug:       Debug.value,
+        delay:       500,
+        htmlMaxCols: 2000,
+        htmlMaxRows: 2000,
+        maxDepth:    3,
+        maxUrls:     10,
+        maxWait:     5000,
+        recursive:   true,
+        userAgent:   'whodis',
     }
 
     if (File.value != "") {
@@ -105,20 +105,18 @@ async function main() {
         }
     }
 
-    for (let u = 0; u < urls.length; u++) {
+    for (let u in urls) {
         let url = urls[u]
 
-	if (url.length > 1) {
-	    if (!url.includes("http://") && !url.includes("https://")) {
-		url = "http://" + url
-            }
+        if (!url.includes("http://") && !url.includes("https://")) {
+            url = "https://" + urls[u]
+        }
 
-	    await new wappalyzer(url, opts).analyze().then(data => {
-		log("crawling '"+ Object.keys(data["urls"])[0] +"'...")
-		let d = parse(data)
-		save(d)
-	    }).catch(err => { console.log(err) })
-	}
+        await new wappalyzer(browser, url, opts).analyze().then(data => {
+            log("crawling '"+ url +"'...")
+            let d = parse(data)
+            save(d)
+        }).catch(err => { console.log(err) })
     }
 
     process.exit(0)
