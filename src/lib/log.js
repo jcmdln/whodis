45c473668519507f41ccbd220e0e88ef1d8c61c0
jcmdln <jcmdln@gmail.com>
2019-01-28 10:06:17 -0500

    Remove empty comments; Improve structure

--- src/lib/log.js
+++ src/lib/log.js
@@ -1,50 +1,33 @@
 // log.js
 
-/**
- * 
- */
 class Log {
-  constructor(Prefix) {
-    this.prefix = Prefix
-  }
+    constructor(Prefix) {
+	this.prefix = Prefix
+    }
 
-  /**
-   * 
-   */
-  Msg(Message) {
-    console.log(this.prefix + ":", Message)
-  }
+    Msg(Message) {
+	console.log(this.prefix + ":", Message)
+    }
 
-  /**
-   * 
-   */
-  Info(Message) {
-    console.log("[INFO]", this.prefix + ":", Message)
-  }
+    Info(Message) {
+	console.log("[INFO]", this.prefix + ":", Message)
+    }
 
-  /**
-   * 
-   */
-  Warning(Message) {
-    console.log("[WARNING]", this.prefix + ":", Message)
-  }
+    Warning(Message) {
+	console.log("[WARNING]", this.prefix + ":", Message)
+    }
 
-  /**
-   * 
-   */
-  Error(Message) {
-    console.log("[ERROR]", this.prefix + ":", Message)
-    process.exit(1)
-  }
+    Error(Message) {
+	console.log("[ERROR]", this.prefix + ":", Message)
+	console.trace()
+	process.exit(1)
+    }
 
-  /**
-   * 
-   */
-  Fatal(Message) {
-    console.log("[FATAL]", this.prefix + ":", Message)
-    process.exit(1)
-    // Try to dump a core?
-  }
+    Fatal(Message) {
+	console.log("[FATAL]", this.prefix + ":", Message)
+	process.exit(1)
+	// Try to dump a core?
+    }
 }
 
 module.exports = Log
--- src/whodis.js
+++ src/whodis.js
@@ -9,105 +9,92 @@ const Wappalyzer = require('wappalyzer')
 const Browser = require('../node_modules/wappalyzer/browsers/zombie.js')
 //const json2csv = require('json2csv').parse
 
+const cmd = new Command("whodis", "[OPTION] URLs...", "Discover software used by websites")
+const log = new Log(cmd.name)
 
-const cmd = new Command("whodis", "[OPTION] URLs...",
-  "Discover software used by websites")
-let Verbose = cmd.Flag("verbose", "V", false,
-  "Show additional messages for tracking execution.")
-let Quiet = cmd.Flag("quiet", "q", false,
-  "Suppress output, ignoring whether --verbose was issued.")
-let Debug = cmd.Flag("debug", "d", false,
-  "Enable Wappalyzer's debug output, ignoring whether --quiet was issued.")
-let File = cmd.Flag("file", "f", "",
-  "Read domains from the specified text file.")
-let Json = cmd.Flag("json", "j", "",
-  "Save data to tje specified JSON file")
+let Verbose = cmd.Flag("verbose", "V", false, "Show additional messages for tracking execution.")
+let Quiet = cmd.Flag("quiet", "q", false, "Suppress output, ignoring whether --verbose was issued.")
+let Debug = cmd.Flag("debug", "d", false, "Enable debug output, ignoring whether --quiet was issued.")
+let File = cmd.Flag("file", "f", "", "Read domains from the specified text file.")
+let Json = cmd.Flag("json", "j", "", "Save data to tje specified JSON file")
 let Args = cmd.Parse()
 
 
-/**
- * 
- */
 function Parse(data) {
-  let parsed = {
-    "url": Object.keys(data.urls)[0]
-  }
-
-  for (let a in data.applications) {
-    let app = data.applications[a]
-
-    if (app.version !== null && app.version !== "") {
-      parsed[app.name] = app.version
-    } else {
-      parsed[app.name] = true
+    let parsed = {
+	"url": Object.keys(data.urls)[0]
     }
-  }
 
-  return JSON.stringify(parsed, null, 2)
+    for (let a in data.applications) {
+	let app = data.applications[a]
+
+	if (app.version !== null && app.version !== "") {
+	    parsed[app.name] = app.version
+	} else {
+	    parsed[app.name] = true
+	}
+    }
+
+    return JSON.stringify(parsed, null, 2)
 }
 
-/**
- * 
- */
 function Save(data) {
-  if (Json.value != "") {
-    if (fs.existsSync(Json.value)) {
-      let file = JSON.parse(
-        fs.readFileSync(Json.value)
-      )
+    if (Json.value != "") {
+	if (fs.existsSync(Json.value)) {
+	    let file = JSON.parse(
+		fs.readFileSync(Json.value)
+	    )
 
-      file.push(data)
-      fs.writeFileSync(Json.value, data + '\n', 'utf8')
+	    file.push(data)
+	    fs.writeFileSync(Json.value, data + '\n', 'utf8')
+	} else {
+	    fs.writeFileSync(Json.value, data + '\n', 'utf8')
+	}
     } else {
-      fs.writeFileSync(Json.value, data + '\n', 'utf8')
+	console.log(data)
     }
-  } else {
-    console.log(data)
-  }
 }
 
-/**
- * 
- */
-async function main() {
-  let urls = []
+async function Get(Urls) {
+    for (let u in Urls) {
+	let url = Urls[u]
 
-  if (File.value != "") {
+	if (!url.includes("http://") && !url.includes("https://")) {
+	    url = "https://" + url
+	}
+
+	await new Wappalyzer(Browser, url, {
+	    debug: Debug.value,
+	    delay: 500,
+	    htmlMaxCols: 2000,
+	    htmlMaxRows: 2000,
+	    maxDepth: 3,
+	    maxUrls: 10,
+	    maxWait: 5000,
+	    recursive: true,
+	    userAgent: 'whodis',
+	}).analyze().then(data => {
+	    let d = Parse(data)
+	    Save(d)
+	}).catch(err => {
+	    console.log(err)
+	})
+    }
+
+    process.exit(0)
+}
+
+
+let urls = []
+
+if (File.value != "") {
     urls = fs.readFileSync(File.value).toString().split("\n")
-  } else {
+} else {
     if (Args.length > 0) {
-      urls = Args
+	urls = Args
     } else {
-      process.exit(1)
+	process.exit(1)
     }
-  }
+}
 
-  for (let u in urls) {
-    let url = urls[u]
-
-    if (!url.includes("http://") && !url.includes("https://")) {
-      url = "https://" + urls[u]
-    }
-
-    await new Wappalyzer(Browser, url, {
-      debug: Debug.value,
-      delay: 500,
-      htmlMaxCols: 2000,
-      htmlMaxRows: 2000,
-      maxDepth: 3,
-      maxUrls: 10,
-      maxWait: 5000,
-      recursive: true,
-      userAgent: 'whodis',
-    }).analyze().then(data => {
-      let d = Parse(data)
-      Save(d)
-    }).catch(err => {
-      console.log(err)
-    })
-  }
-
-  process.exit(0)
-};
-
-main()
+Get(urls)
